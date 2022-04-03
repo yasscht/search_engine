14ce354ba3cab172298c17d103fee91ae06fa96c
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-03 22:57:39 +0000

    add the input field to the app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Search from "./Search";
 const Navbar = ({ darkMode, setDarkMode }) => (
-  <div className="p-5 pb-0 flex flex-wrap sm:justify-beetween justify-center items-center border-b dark:border-gray-700 border-gray-200">
+  <div className="p-5 pb-0 flex  flex-wrap sm:justify-beetween  items-center border-b dark:border-gray-700 border-gray-200">
     <div className="flex justify-between items-center space-x-5 w-screen">
       <Link to="/">
         <p className="text-2xl mb-2 bg-blue-500 font-bold text-white py-1 px-2  rounded dark:bg-gray-50 dark:text-gray-500">

7284993ae0a9d2988fb24df440ed86fc0fffd8c2
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-03 17:51:40 +0000

    implement the search feature

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,9 +7,48 @@ import Loading from "./Loading";
 
 const Result = () => {
   const { results, isLoading, getResults, searchTerm } = useResultContext();
-
+  useEffect(() => {
+    getResults("/search/q=facebook&num=40");
+  }, []);
   const location = useLocation();
+  console.log(results);
   if (isLoading) return <Loading />;
+  switch (location.pathname) {
+    case "/search":
+      return (
+        <div className="sm:px-56 flex flex-wrap justify-between space-y-6">
+          {results?.results?.map(({ link, title, description }, index) => (
+            <div key={index} className="md:w-2/5 w-full">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm">
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700  ">
+                  {title}
+                </p>
+                <p className="text-lg hover:underline dark:text-white-300 text-black-700  ">
+                  {description}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
+      break;
+    case "/news":
+      return "videos";
+      break;
+    case "/videos":
+      return "videos";
+      break;
+    case "/images":
+      return "images";
+      break;
+
+    default:
+      return "Error";
+      break;
+  }
   return <div>result</div>;
 };
 export default Result;

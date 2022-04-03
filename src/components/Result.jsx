718f298e115508ea41ad2b85c37bd6128660128f
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-03 18:38:27 +0000

    implementation of the search news feature

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -19,6 +19,7 @@ const Result = () => {
   }, [searchTerm, location.pathname]);
 
   console.log(results);
+  if (isLoading) return <Loading />;
 
   switch (location.pathname) {
     case "/search":
@@ -43,7 +44,35 @@ const Result = () => {
       );
       break;
     case "/news":
-      return "videos";
+      return (
+        <div className="sm:px-56 flex flex-wrap justify-between items-center space-y-6">
+          {results?.entries?.map(({ id, links, source, title }) => (
+            <div key={id} className="md:w-2/5 w-full ">
+              <a
+                href={links?.[0].href}
+                target="_blank"
+                rel="noreferrer "
+                className="hover:underline "
+              >
+                <p className="text-lg dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+              </a>
+              <div className="flex gap-4">
+                <a
+                  href={source?.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:underline hover:text-blue-300"
+                >
+                  {" "}
+                  {source?.href}
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
       break;
     case "/videos":
       return "videos";

d540c224b2352b2b8d4d917ecd4afd5dbb99259b
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-03 17:23:32 +0000

    wrap the entire app with the context prpovider

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import ReactPlayer from "react-player";
+import { useLocation } from "react-router-dom";
+import { useResultContext } from "../context/ResultContextProvider";
+
+import Loading from "./Loading";
+
 const Result = () => {
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
+
+  const location = useLocation();
+  if (isLoading) return <Loading />;
   return <div>result</div>;
 };
 export default Result;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ResultContextProvider } from "./context/ResultContextProvider";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ResultContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ResultContextProvider>,
   document.getElementById("root")
 );
 
